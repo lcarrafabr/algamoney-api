e16903ddfaf41c8e18d85072bc126fea6b2ecc31
lcarrafabr <lcarrafa.br@gmail.com>
2019-12-19 20:13:01 -0300

    Validando valores inválidos com Bean Validation

--- algamoney-api/src/main/java/com/example/algamoney/api/model/Categoria.java
+++ algamoney-api/src/main/java/com/example/algamoney/api/model/Categoria.java
@@ -1,11 +1,12 @@
 package com.example.algamoney.api.model;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Entity
 @Table(name = "categoria")
@@ -15,7 +16,8 @@ public class Categoria {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long codigo;
 
-	@Column(length=200)
+	@NotNull
+	@Size(min = 3, max = 50)
 	private String nome;
 
 	public Long getCodigo() {
