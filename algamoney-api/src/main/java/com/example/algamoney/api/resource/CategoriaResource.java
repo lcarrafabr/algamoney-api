e9cb41c90c9118760149e61b860280d80ab3df17
lcarrafabr <lcarrafa.br@gmail.com>
2019-12-20 21:43:54 -0300

    usando eventos para adicionar header location

--- algamoney-api/src/main/java/com/example/algamoney/api/resource/CategoriaResource.java
+++ algamoney-api/src/main/java/com/example/algamoney/api/resource/CategoriaResource.java
@@ -8,6 +8,8 @@ import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +19,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.example.algamoney.api.event.RecursoCriadoEvent;
 import com.example.algamoney.api.model.Categoria;
 import com.example.algamoney.api.repository.CategoriaRepository;
 
@@ -26,6 +29,9 @@ public class CategoriaResource {
 	
 	@Autowired
 	private CategoriaRepository categoriaRepository;
+	
+	@Autowired
+	private ApplicationEventPublisher publisher;
 	
 	@GetMapping
 	public List<Categoria> listar(){
@@ -39,14 +45,17 @@ public class CategoriaResource {
 		
 		Categoria categoriaSalva =  categoriaRepository.save(categoria);
 		
+		
+		/**Foi criado um metodo para pegar os evetos (Classe recursCriadoEvent)*/
 		/**ServletUriComponentsBuilder irá pegar a partir da uri da requisição atual, adicionar o codigo e setar o header Location
 		 * o URI*/
-		URI uri =  ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}")
-		.buildAndExpand(categoriaSalva.getCodigo()).toUri();
+		//URI uri =  ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}")
+		//.buildAndExpand(categoriaSalva.getCodigo()).toUri();
+		//response.setHeader("Location", uri.toASCIIString());
 		
-		response.setHeader("Location", uri.toASCIIString());
+		publisher.publishEvent(new RecursoCriadoEvent(this, response, categoriaSalva.getCodigo()));
 		
-		return ResponseEntity.created(uri).body(categoriaSalva);
+		return ResponseEntity.status(HttpStatus.CREATED).body(categoriaSalva);
 	}
 	
 	@GetMapping("/{codigo}")
