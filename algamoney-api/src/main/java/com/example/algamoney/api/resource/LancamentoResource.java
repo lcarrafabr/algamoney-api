4be7a01856613bf58141c858183585379d3830af
lcarrafabr <lcarrafa.br@gmail.com>
2019-12-26 20:14:12 -0300

    Criando filtros de lançamentos (O metaModel não funcionou)

--- algamoney-api/src/main/java/com/example/algamoney/api/resource/LancamentoResource.java
+++ algamoney-api/src/main/java/com/example/algamoney/api/resource/LancamentoResource.java
@@ -25,6 +25,7 @@ import com.example.algamoney.api.event.RecursoCriadoEvent;
 import com.example.algamoney.api.exceptionhandler.AlgamoneyExceptionHandler.Erro;
 import com.example.algamoney.api.model.Lancamento;
 import com.example.algamoney.api.repository.LancamentoRepository;
+import com.example.algamoney.api.repository.filter.LancamentoFilter;
 import com.example.algamoney.api.service.LancamentoService;
 import com.example.algamoney.api.service.exception.PessoaInexistenteOuInativaException;
 
@@ -44,9 +45,16 @@ public class LancamentoResource {
 	@Autowired
 	private MessageSource messageSource;
 	
+	/**
 	@GetMapping
-	public List<Lancamento> listar() {
+	public List<Lancamento> pesquisar() {
 		return lancamentoRepository.findAll();
+	}
+	*/
+	
+	@GetMapping
+	public List<Lancamento> pesquisar(LancamentoFilter lancamentoFilter) {
+		return lancamentoRepository.filtrar(lancamentoFilter);
 	}
 	
 	
