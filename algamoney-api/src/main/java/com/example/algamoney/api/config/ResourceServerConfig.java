4b7c531728c3c9a78db541e5f066005349992bd5
lcarrafabr <lcarrafa.br@gmail.com>
2020-05-26 20:56:35 -0300

    Não valida basic authorization

--- algamoney-api/src/main/java/com/example/algamoney/api/config/ResourceServerConfig.java
+++ algamoney-api/src/main/java/com/example/algamoney/api/config/ResourceServerConfig.java
@@ -3,6 +3,7 @@ package com.example.algamoney.api.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.access.expression.method.MethodSecurityExpressionHandler;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -17,6 +18,7 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.R
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.expression.OAuth2MethodSecurityExpressionHandler;
 
+@Profile("oauth-security")
 @Configuration  // A anotação @EnableWebSecurity já possui a @Configuration. Mas coloco para lembrar que é uma classe de configuração
 @EnableWebSecurity
 @EnableResourceServer //<<<< Incluir para implementar o oauth2
@@ -51,11 +53,11 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter{
 		http.authorizeRequests()
 		.antMatchers("/categorias").permitAll()
 		.anyRequest().authenticated()
-	.and()
-	.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) //Sem manter sessão
-	.and()
-	.csrf().disable();
-		
+		.and()
+		.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) //Sem manter sessão
+		.and()
+		.csrf().disable();
+			
 		
 		/**versão antiga antes do oauth
 		
--- algamoney-api/src/main/java/com/example/algamoney/api/cors/CorsFilter.java
+++ algamoney-api/src/main/java/com/example/algamoney/api/cors/CorsFilter.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
@@ -46,6 +47,14 @@ public class CorsFilter implements Filter{
 			chain.doFilter(req, resp);
 		}
 		
+	}
+	
+	@Override
+	public void destroy() {
+	}
+
+	@Override
+	public void init(FilterConfig arg0) throws ServletException {
 	}
 
 }
