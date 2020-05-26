be40b28d7406be834c6465050c2963f2262b7dfd
lcarrafabr <lcarrafa.br@gmail.com>
2020-05-26 19:53:57 -0300

    Erro de CORS

--- algamoney-api/src/main/java/com/example/algamoney/api/config/property/AlgamoneyApiProperty.java
+++ algamoney-api/src/main/java/com/example/algamoney/api/config/property/AlgamoneyApiProperty.java
@@ -5,7 +5,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 @ConfigurationProperties("algamoney") // <<< Após salvar mandar add no poom.xml o spring-boot-configuration-processor. (clica no sinal amarelo)
 public class AlgamoneyApiProperty {
 
-	private String originPermitida = "http://localhost:8000";
+	private String originPermitida = "http://localhost:4200";
 	
 	private final Seguranca seguranca = new Seguranca();
 
