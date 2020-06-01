91122e7574176112f1305580de0e7e1205903c6d
lcarrafabr <lcarrafa.br@gmail.com>
2020-06-01 08:10:46 -0300

    Pesquisar pessoas retornando pessoas (Pesquisar não funciona)

--- algamoney-api/src/main/java/com/example/algamoney/api/resource/PessoaResource.java
+++ algamoney-api/src/main/java/com/example/algamoney/api/resource/PessoaResource.java
@@ -8,6 +8,8 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -18,6 +20,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -39,12 +42,12 @@ public class PessoaResource {
 	@Autowired
 	private PessoaService pessoaService;
 	
-	@GetMapping
-	@PreAuthorize("hasAuthority('ROLE_PESQUISAR_PESSOA')")
-	public List<Pessoa> listar(){
-		
-		return pessoaRepository.findAll();
-	}
+//	@GetMapping
+//	@PreAuthorize("hasAuthority('ROLE_PESQUISAR_PESSOA')")
+//	public List<Pessoa> listar(){
+//		
+//		return pessoaRepository.findAll();
+//	}
 	
 	@PostMapping
 	@PreAuthorize("hasAuthority('ROLE_CADASTRAR_PESSOA')")
@@ -90,6 +93,12 @@ public class PessoaResource {
 	public void atualizarPropriedadeAtivo(@PathVariable Long codigo, @RequestBody Boolean ativo) {
 		
 		pessoaService.atualizarPropriedadeAtivo(codigo, ativo);
+	}
+	
+	@GetMapping
+	@PreAuthorize("hasAuthority('ROLE_PESQUISAR_PESSOA')")
+	public Page<Pessoa> pesquisar2(@RequestParam(required = false, defaultValue = "") String nome, Pageable pageable) {
+		return pessoaRepository.findByNomeContaining(nome, pageable);
 	}
 	
 
